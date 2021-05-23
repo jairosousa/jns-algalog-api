ad62ec0186af37cb1fe88ce56136854a94adf44c
Jairo <jaironsousa@gmail.com>
2021-05-23 11:18:34 -0300

    :construction: Implementando o CRUD de cliente

--- src/main/java/com/algaworks/algalog/algalogapi/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/algalogapi/controller/ClienteController.java
@@ -4,9 +4,17 @@ import com.algaworks.algalog.algalogapi.domain.model.Cliente;
 import com.algaworks.algalog.algalogapi.domain.repository.ClienteRepository;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
@@ -16,13 +24,55 @@ public class ClienteController {
 
   private final ClienteRepository clienteRepository;
 
- @GetMapping
+  @GetMapping
   public List<Cliente> listar() {
     return clienteRepository.findAll();
   }
 
-  @GetMapping("find")
+  @GetMapping("/find")
   public List<Cliente> findByNome(@RequestParam String nome) {
-   return clienteRepository.findByNomeContaining(nome);
+    return clienteRepository.findByNomeContaining(nome);
   }
+
+  @GetMapping("/{clienteId}")
+  public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+
+    return clienteRepository.findById(clienteId)
+//        .map(cliente -> ResponseEntity.ok(cliente))
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Cliente adicionar(@RequestBody Cliente cliente) {
+    return clienteRepository.save(cliente);
+  }
+
+  @PutMapping("/{clienteId}")
+  public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+      @RequestBody Cliente cliente) {
+
+    if (!clienteRepository.existsById(clienteId)) {
+      return ResponseEntity.notFound().build();
+    }
+
+    cliente.setId(clienteId);
+    cliente = clienteRepository.save(cliente);
+
+    return ResponseEntity.ok(cliente);
+  }
+
+  @DeleteMapping("/{clienteId}")
+  public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+    if (!clienteRepository.existsById(clienteId)) {
+      return ResponseEntity.notFound().build();
+    }
+
+    clienteRepository.deleteById(clienteId);
+
+    return ResponseEntity.noContent().build();
+  }
+
 }
